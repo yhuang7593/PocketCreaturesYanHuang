b12a6f6cf30014f0fddb0897a21be3897db70f0a
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2018-12-11 08:40:05 -0500

    Initial commit

---
+++ src/Rooms/House.java
@@ -0,0 +1,29 @@
+package Rooms;
+
+import Game.Runner;
+import People.Person;
+
+public class House extends Room{
+
+	public House(int x, int y) {
+        super(x, y);
+
+    }
+
+        /**
+         * Triggers the game beginning conditions.
+         * @param x the Person entering
+         */
+        @Override
+        public void enterRoom(Person x) {
+
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("Welcome to the house. Before you begin, choose your starter creature. ,
+
+
+    }
+
+
+    }
